7a46a516eae4dac6d5571f688e0b869bffc3e303
rahma <rahma@pop-os.localdomain>
2020-08-05 11:50:43 +0700

    edit home page part 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,44 +2,56 @@
 
 @section('content')
 
-<header class="masthead text-center text-white">
+<header class="masthead ">
     <div class="masthead-content">
       <div class="container">
-        <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-        <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
-        <a href="#catalog" class="btn btn-yellow btn-xl rounded-pill mt-5">Learn More</a>
+        <div class="row">
+          <div class="col-lg-6 order-lg-2 text-center">
+            <div class="p-5">
+              <img class="img-header" src="{{ ('assets/images/read.svg') }}"> 
+            </div>
+          </div>
+          <div class="col-lg-6 order-lg-1">
+            <div class="p-5">
+              <h2 class="display-4">BBA - Bahagia Bersama AlQuran</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+              <a href="#catalog" class="btn btn-yellow">Let's Buy!</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </header>
 
+
   <section id="catalog">
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-lg-6 order-lg-2">
+        <div class="col-lg-4 order-lg-2">
           <div class="p-5">
-            <img class="img-fluid" src="{{ ('assets/images/bba2.jpg') }}" > 
+            <img class="img-catalog" src="{{ ('assets/images/cover-book.jpg') }}" > 
           </div>
         </div>
-        <div class="col-lg-6 order-lg-1">
+        <div class="col-lg-8 order-lg-1">
           <div class="p-5">
-            <h2 class="display-4">BBA - Bahagia Bersama AlQuran</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+            <h4 class="display-4">BBA - Bahagia Bersama AlQuran</h4>            
             <a href="" class="btn btn-outline-yellow">Warning</a>
           </div>
         </div>
       </div>
     </div>
   </section>
+  <hr>
 
   <section>
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-lg-6">
+        <div class="col-lg-4">
           <div class="p-5">
-            <img class="img-fluid" src="{{ ('assets/images/bba3.jpg') }}" > 
+            <img class="img-catalog" src="{{ ('assets/images/cover-book.jpg') }}" > 
           </div>
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-8">
           <div class="p-5">
             <h2 class="display-4">QM - Quranic Motivation</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
@@ -49,24 +61,6 @@
       </div>
     </div>
   </section>
-
-  <section>
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-6 order-lg-2">
-          <div class="p-5">
-            <img class="img-fluid" src="{{ ('assets/images/bba1.jpg') }}" > 
-          </div>
-        </div>
-        <div class="col-lg-6 order-lg-1">
-          <div class="p-5">
-            <h2 class="display-4">RBA - Rihlah Bersama AlQuran</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-            <a href="" class="btn btn-outline-yellow">Warning</a>  
-        </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <hr>
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,8 +14,8 @@
 
 	<nav class="navbar navbar-expand-lg navbar-light navbar-custom">
     <div class="container">
-	  <a class="navbar-brand" href="{{ route('home') }}"> Muezza Books!
-		{{-- <img class="img-nav" src="{{ ('assets/images/logo-muezza.png') }}" > --}}
+	  <a class="navbar-brand" href="{{ route('home') }}">
+		<img class="img-nav" src="{{ ('assets/images/logo-muezza.png') }}" >
 	 </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
