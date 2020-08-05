513b7688c24ee118d12967ba851e1d72d6eeba12
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2020-08-05 12:41:01 +0700

    add login

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@home')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/catalog', 'CatalogController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
