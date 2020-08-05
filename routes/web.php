5537ccf94df4a5d5f9d5f908d2dec36146158f72
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2020-08-05 12:48:57 +0700

    add login pages and controller

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@home')->name('home');
 Route::get('/catalog', 'CatalogController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
