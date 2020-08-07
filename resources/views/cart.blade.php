79de1b540b2a495ab781c1009f0102e9f87bef12
kelvin-azaria <kelvin.azaria.billy@gmail.com>
2020-08-07 13:43:20 +0700

    Polish cart page

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,73 +1,71 @@
-@extends('layout')
 
+
+@extends('layout')
 @section('content')
 <section>
-    <div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <div class="p-3">
-            <div class="card">
-                <div class="card-body">
+      <div class="col-md-12">
+        <div class="p-3">
+          <div class="card">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-8">
+                  <div class="card-body">
                     <div class="row">
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-2">
-                                        <img src="{{ ('assets/images/cover-book.jpg') }}" class="img-card" alt="">
-                                    </div>
-                                    <div class="col-md-8">
-                                        <h4>Bahagia Bersama Al-Quran</h4>
-                                        <form>
-                                            <div class="form-group">
-                                              <input type="number" class="form-control" id="number" value="2">
-                                            </div>
-                                          </form>
-                                          <p>Total Harga : Rp 190000</p>
-                                    </div>
-                                    <div class="col-md-2">
-                                            
-                                        <a href="3" class="btn btn-yellow btn-delete"> hapus</a>
-                                      </div>
-                                </div>                          
-                            </div>
-                            <hr>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-2">
-                                    <img src="{{ ('assets/images/cover-book.jpg') }}" class="img-card" alt="">
-                                </div>
-                                <div class="col-md-8">
-                                    <h4>Bahagia Bersama Al-Quran</h4>
-                                    <form>
-                                        <div class="form-group">
-                                          <input type="number" class="form-control" id="number" value="2">
-                                        </div>
-                                      </form>
-                                      <p>Total Harga : Rp 190000</p>
-                                </div>
-                                <div class="col-md-2">
-                                        
-                                    <a href="3" class="btn btn-yellow btn-delete"> hapus</a>
-                                  </div>
-                            </div>                          
-                        </div>
-                        </div>
-
-                        <div class="col-md-4">
-                            <div class="card">
-                                <div class="card-body">
-                                  <h5 class="card-title">Grand Total : 180.000</h5>
-                                 <a href="#" class="btn btn-yellow btn-block">Go to Payment</a>
-                                </div>
-                              </div>
-                            </div>
-                        </div>
+                      <div class="col-md-2 text-center">
+                        <img src="{{ ('assets/images/cover-book.jpg') }}" class="img-fluid pb-3" style="max-height: 200px">
+                      </div>
+                      <div class="col-md-8">
+                        <h4>Bahagia Bersama Al-Quran</h4>
+                        <form>
+                          <div class="form-group">
+                            <input type="number" class="form-control" id="number" value="2">
+                          </div>
+                        </form>
+                        <p>Total Harga : Rp 190000</p>
+                      </div>
+                      <div class="col-md-2 d-flex">
+                        <a href="3" class="btn btn-danger mt-auto">Hapus</a>
+                      </div>
                     </div>
+                  </div>
+                  <hr>
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-md-2">
+                        <img src="{{ ('assets/images/cover-book.jpg') }}" class="img-fluid pb-3" style="max-height: 200px">
+                      </div>
+                      <div class="col-md-8">
+                        <h4>Bahagia Bersama Al-Quran</h4>
+                        <form>
+                          <div class="form-group">
+                            <input type="number" class="form-control" id="number" value="2">
+                          </div>
+                        </form>
+                        <p>Total Harga : Rp 190000</p>
+                      </div>
+                      <div class="col-md-2 d-flex">
+                        <a href="3" class="btn btn-danger mt-auto">Hapus</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Grand Total : 180.000</h5>
+                      <a href="#" class="btn btn-yellow btn-block">Go to Payment</a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-  </section>
+  </div>
+</section>
 @endsection
+
