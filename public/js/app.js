5c1581910c23fb99969ca7362df30a0f27e6c47f
rahma <rahma@pop-os.localdomain>
2020-09-22 16:08:53 +0700

    add tabs to detail product page

--- public/js/app.js
+++ public/js/app.js
@@ -515,8 +515,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/kelv/Documents/MuezzaBooks/Project/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/kelv/Documents/MuezzaBooks/Project/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/rahma/Documents/muezzabooks/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/rahma/Documents/muezzabooks/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
